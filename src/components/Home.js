0eae4b1bd2113f00902fbb81f89bd4aec4e981bf
dan <dancr.wley@gmail.com>
2023-04-28 20:46:10 -0400

    gren

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import city from "../data/user"
+import username from "../data/user.js"
 
 function Home() {
   return (
@@ -9,3 +11,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home;
